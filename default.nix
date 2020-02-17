97e539ddbe131f02ab1e436dcbd8a1ac171af6ce
Trevor Riles <trevor@trevorriles.com>
2020-02-16 20:08:14 -0600

    add iosevka-nerd-font to person NUR

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  iosevka-nerd-font = pkgs.callPackage ./pkgs/iosevka-nerd-font { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
